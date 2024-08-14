9d6f005b9554d57c6d33a1ae64e2e16f0bf0e80a
Juan Alday <juanaldayparejo@gmail.com>
2024-08-14 10:02:50 +0100

    try changing conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,10 +37,10 @@ extensions = [
 
 #Defining each image shown in the gallery
 nbsphinx_thumbnails = {
-    'examples/mars_solocc/mars_SO': '_static/exomars_SO.jpg',
-    'examples/Jupiter_CIRS_nadir_thermal_emission/Jupiter_CIRS': '_static/jupiter_cassini.jpg',
-    'examples/Measurement/Measurement': '_static/observation_sketch.png',
-    'examples/Stellar/StellarExample': '_static/solar_spec.jpg',
+    #'examples/mars_solocc/mars_SO': '_static/exomars_SO.jpg',
+    #'examples/Jupiter_CIRS_nadir_thermal_emission/Jupiter_CIRS': '_static/jupiter_cassini.jpg',
+    #'examples/Measurement/Measurement': '_static/observation_sketch.png',
+    #'examples/Stellar/StellarExample': '_static/solar_spec.jpg',
     'examples/atmosphere_tutorial/atmosphere_tutorial': '_static/planetary_atmospheres.png',
     'examples/makephase/run_makephase': '_static/mars_sunset2.jpg',
     'examples/surface_modes/surface': '_static/mars_surface.png'
@@ -48,6 +48,5 @@ nbsphinx_thumbnails = {
 
 #Defining the actual appearance of the website
 html_theme = 'sphinx_rtd_theme'
-
 
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
