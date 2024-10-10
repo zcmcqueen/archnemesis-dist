903e3ebeb7335fa4acf6c931d4294fecb715104d
Juan Alday <juanaldayparejo@gmail.com>
2024-10-10 16:41:13 +0100

    adding pymultinest as a required install

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='archnemesis',
       version='1.0.0',
       description='Python implementation of the NEMESIS radiative transfer code',
       packages=['archnemesis'],
-      install_requires=['numpy','matplotlib','sympy','numba','scipy'],
+      install_requires=['numpy','matplotlib','sympy','numba','scipy','pymultinest'],
 #      ext_modules=[ext1],
       )
