1ed0f1ecfa23d370e3de623e749d2616d707f918
Juan Alday <juanaldayparejo@gmail.com>
2025-02-04 08:14:55 +0000

    add required numba version in setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,17 @@ from setuptools import setup
 
 setup(name='archnemesis',
       version='1.0.0',
-      description='Python implementation of the NEMESIS radiative transfer code',
+      description='Python implementation of the NEMESIS radiative transfer and retrieval code',
       packages=['archnemesis'],
-      install_requires=['numpy','matplotlib','numba','scipy','pymultinest','cdsapi','pygrib','joblib','h5py','basemap'],
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'numba>=0.57.0',
+            'scipy',
+            'pymultinest',
+            'cdsapi',
+            'pygrib',
+            'joblib',
+            'h5py',
+            'basemap'],
       )
